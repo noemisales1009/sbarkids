7e92ed6157c9271a1d94eea37bfc54c4ec3e88be
noemisales1009 <143117681+noemisales1009@users.noreply.github.com>
2025-12-17 21:35:08 -0300

    fix: update tailwind config paths and add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ export default {
   content: [
     "./index.html",
     "./App.tsx",
+    "./index.tsx",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
