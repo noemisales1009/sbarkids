45c9ee5b938e61c8ad37f411f16cb1c9b7ff0981
noemisales1009 <143117681+noemisales1009@users.noreply.github.com>
2025-12-17 21:38:57 -0300

    fix: correct CSS import path to use relative path

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'src/index.css';
+import './src/index.css';
 
 // Capturar erros globais de carregamento
 console.log('🚀 Iniciando aplicação SBAR Juju...');
